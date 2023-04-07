18882d71b76f2a31b798d05f5570104b7beaad1a
Jacob Rathbun <jrathofbun@gmail.com>
2023-04-06 23:32:27 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//function for distance greeting
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!';
+  }
+  else if (distance >= 401 && distance < 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//function for ternary city check
+function ternaryCheckCity(city){
+  const cityMessage = city == 'NYC' ? 'Ok, sounds good.' : 'No go.'  
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//function for case switch tip response
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
